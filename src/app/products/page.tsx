8daf905494bc91f8df3264659e726259c7fec3e9
Mahedul-Karim <karimrupo21@gmail.com>
2025-08-22 17:22:09 +0600

    products page typo fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -12,7 +12,7 @@ const Page = async () => {
     <div className="l-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 my-12">
       {products.map((product: any) => (
         <Card
-          key={product.id}
+          key={product._id}
           className="group hover:shadow-medium transition-all duration-300 border-0 bg-card pt-0"
         >
           <div className="relative aspect-square overflow-hidden rounded-t-lg">
